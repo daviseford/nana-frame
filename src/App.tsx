4b5d3afd3468dd7258a820c0dfaf23eabc5e5661
Davis Ford <davis.e.ford@gmail.com>
2020-05-22 19:50:18 -0400

    ah

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import 'core-js/stable' // polyfills
+import "core-js/stable"; // polyfills
 import React, { useEffect, useState } from "react";
 
 import { getSlideshowFiles, IAlbum } from "./creds/aws";
@@ -38,19 +38,6 @@ function App() {
       <Album album={album} />
 
       {(!album || !album.photos.length) && <Error />}
-
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
     </>
   );
 }
