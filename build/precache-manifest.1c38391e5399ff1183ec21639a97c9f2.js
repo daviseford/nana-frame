80116d0a2c153b6b0f2363491c9035b30cf07ed5
Davis Ford <davis.e.ford@gmail.com>
2020-05-22 16:05:18 -0400

    add build

---
+++ build/precache-manifest.1c38391e5399ff1183ec21639a97c9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef6e7ce5e70ad14293b2f217af6ae98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04dee0294d49ace72ae7",
+    "url": "/static/css/main.d9e421fd.chunk.css"
+  },
+  {
+    "revision": "e331a938e85f785d78d1",
+    "url": "/static/js/2.a5cfa635.chunk.js"
+  },
+  {
+    "revision": "21c75a98ff1b0cc08c27fd17e11b8c68",
+    "url": "/static/js/2.a5cfa635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dee0294d49ace72ae7",
+    "url": "/static/js/main.653fbf3c.chunk.js"
+  },
+  {
+    "revision": "0488cba0e6b6e9134890",
+    "url": "/static/js/runtime-main.08a3d46a.js"
+  }
+]);
