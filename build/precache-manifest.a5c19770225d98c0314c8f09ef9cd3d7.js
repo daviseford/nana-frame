6794fbab74b874e679ad6b15bf8affac307905a6
Davis Ford <davis.e.ford@gmail.com>
2020-05-24 17:15:56 -0400

    build

---
+++ build/precache-manifest.a5c19770225d98c0314c8f09ef9cd3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05c371aaf7094e76e1121a7427c1545",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45a8a3d699a073b0028b",
+    "url": "./static/css/main.44a6b2c0.chunk.css"
+  },
+  {
+    "revision": "8fba1dac8f89cddf425e",
+    "url": "./static/js/2.e3ee1999.chunk.js"
+  },
+  {
+    "revision": "65eb0436089e4b6a160bdec60c4f4815",
+    "url": "./static/js/2.e3ee1999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a8a3d699a073b0028b",
+    "url": "./static/js/main.5fa9c746.chunk.js"
+  },
+  {
+    "revision": "5268766c04268d7125d7",
+    "url": "./static/js/runtime-main.4154c04c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.28009301cbc45ae3adac202cf715c6ce.js"
+  "./precache-manifest.a5c19770225d98c0314c8f09ef9cd3d7.js"
 );
 
 self.addEventListener('message', (event) => {
