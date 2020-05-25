ff1a309f29af10269efc24be28185e364915d534
Davis Ford <davis.e.ford@gmail.com>
2020-05-25 13:05:57 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -10,7 +10,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-DISPLAY=:0 chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080/build/
+DISPLAY=:0 chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080/build/ &
 
 sudo /usr/bin/chromium-browser &
 python3 -m http.server 8080
