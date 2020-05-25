6c7385e2cb73245e8d703240a26a6cf43fa2087d
Davis Ford <davis.e.ford@gmail.com>
2020-05-25 14:15:18 -0400

    Update aws.ts

--- src/creds/aws.ts
+++ src/creds/aws.ts
@@ -1,12 +1,13 @@
 import AWS from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 
+const Bucket = "nana-media";
+const Region = "us-east-1";
 // Initialize the Amazon Cognito credentials provider
-AWS.config.region = "us-east-1"; // Region
+AWS.config.region = Region;
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: "us-east-1:3a445c96-1f34-4459-b18c-91f99b16cbf4",
 });
-
-const Bucket = "nana-media";
 
 // Create a new service object
 const s3 = new AWS.S3({
@@ -18,19 +19,58 @@ export interface IAlbum {
   photos: string[]; // urls
 }
 
+type TData = PromiseResult<AWS.S3.ListObjectsV2Output, AWS.AWSError>;
+
+let truncatedData: TData[] = [];
+
+const truncatedMapper = (x: typeof truncatedData): TData["Contents"][] => {
+  return x.map((y) => y.Contents).filter((z) => !!z);
+};
+
+const getTruncated = async (data: TData): Promise<TData["Contents"][]> => {
+  const {
+    Prefix,
+    NextContinuationToken: ContinuationToken,
+    IsTruncated,
+  } = data;
+
+  if (!IsTruncated || !ContinuationToken) return truncatedMapper(truncatedData);
+
+  try {
+    const newData = await s3
+      .listObjectsV2({ Prefix, Bucket, ContinuationToken })
+      .promise();
+
+    if (!newData || !newData.Contents) return truncatedMapper(truncatedData);
+
+    truncatedData.push(newData);
+    return getTruncated(newData);
+  } catch (err) {
+    console.log(err);
+    return truncatedMapper(truncatedData);
+  }
+};
+
 // Show the photos that exist in an album.
 export const getSlideshowFiles = async (): Promise<IAlbum | void> => {
   const imgPath = "uploads/";
+  const href = "https://nana-media.s3.amazonaws.com/";
   try {
     const data = await s3.listObjectsV2({ Prefix: imgPath, Bucket }).promise();
-    const href = "https://nana-media.s3.amazonaws.com/";
+
+    const truncatedEntries = await getTruncated(data);
 
     if (!data.Contents) throw new Error();
 
-    const photos = data.Contents.map((photo) => {
-      const photoKey = photo.Key as string;
-      return href + photoKey;
-    }).filter((x) => x !== "" && x !== href + imgPath);
+    const contents = [data.Contents, ...truncatedEntries].flat();
+
+    const photos = contents
+      .map((photo) => {
+        if (!photo) return "";
+        const photoKey = photo.Key as string;
+        return href + photoKey;
+      })
+      .filter((x) => x !== "" && x !== href + imgPath);
 
     return { photos };
   } catch (err) {
