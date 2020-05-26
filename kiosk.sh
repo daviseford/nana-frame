839965f5dfeb5e57e35cc608e7124ed07db86ffe
Davis Ford <davis.e.ford@gmail.com>
2020-05-26 15:28:58 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -12,8 +12,9 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromi
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
 
-python3 -m http.server 8080 --directory=${pwd} &
+python3 -m http.server 8080 --directory=$(pwd) &
 
 sleep 5
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080/build/ &
+# /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080/build/ &
+/usr/bin/chromium-browser --noerrdialogs http://localhost:8080/build/ &
