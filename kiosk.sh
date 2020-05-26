65d4c731aded111bc064d14548aa2d9d9fe050b7
Davis Ford <davis.e.ford@gmail.com>
2020-05-26 15:38:50 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -12,10 +12,11 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-
 python3 -m http.server 8080 --directory=${BASEDIR} &
 
-sleep 5
+sleep 3
 
 # /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8080/build/ &
 /usr/bin/chromium-browser --noerrdialogs http://localhost:8080/build/ &
+
+lxterminal -e sudo raspi-config
